5f918397eaa50c56b15f9475dd44f0dc4ec40eda
Martin Trojer <martin.trojer@gmail.com>
2014-08-01 17:45:33 +0100

    back on snapshot

--- joplin.jdbc/project.clj
+++ joplin.jdbc/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.jdbc "0.1.9"
+(defproject joplin.jdbc "0.1.10-SNAPSHOT"
   :description "JDBC support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [joplin.core "0.1.9"]
+                 [joplin.core "0.1.10-SNAPSHOT"]
                  [ragtime "0.3.6"]])
--- joplin.lein/project.clj
+++ joplin.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.lein "0.1.9"
+(defproject joplin.lein "0.1.10-SNAPSHOT"
   :description "Joplin Leiningen plugin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
