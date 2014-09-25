85d2918c0ce5cc657498c11e3ee29d8b9dcee0ad
Martin Trojer <martin.trojer@gmail.com>
2014-09-25 10:34:39 +0100

    bump datomic version

--- joplin.datomic/project.clj
+++ joplin.datomic/project.clj
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.datomic/datomic-free "0.9.4815.12"]
+                 [com.datomic/datomic-free "0.9.4899"]
                  [joplin.core "0.1.13-SNAPSHOT"]])
