08533b88555f1c53b40441d1ee98136d86948c6c
Michael Klishin <michael@novemberain.com>
2014-11-15 01:56:00 +0300

    [elasticsearch] Elastisch 2.1.0-rc1

--- joplin.elasticsearch/project.clj
+++ joplin.elasticsearch/project.clj
@@ -6,6 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/elastisch "2.1.0-beta7"]
+                 [clojurewerkz/elastisch "2.1.0-rc1"]
                  [joplin.core "0.2.3-SNAPSHOT"]
                  [clj-time "0.8.0"]])
