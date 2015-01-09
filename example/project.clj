bf5bab5658080d992a047b15e49cba9ffcdc21a9
Martin Trojer <martin.trojer@gmail.com>
2015-01-09 18:35:44 +0000

    v0.2.5

--- example/project.clj
+++ example/project.clj
@@ -1,7 +1,7 @@
-(defproject joplin-example "0.2.5-SNAPSHOT"
+(defproject joplin-example "0.2.5"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.h2database/h2 "1.3.160"]]
-  :plugins [[joplin.lein "0.2.5-SNAPSHOT"]]
+  :plugins [[joplin.lein "0.2.5"]]
 
   :source-paths ["src" "joplin"]
 
