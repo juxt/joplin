8d66be7f26fefadb14c63a47a46481078aa6512d
Martin Trojer <martin.trojer@gmail.com>
2015-02-13 13:32:18 +0000

    v0.2.8

--- joplin.lein/project.clj
+++ joplin.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.lein "0.2.8-SNAPSHOT"
+(defproject joplin.lein "0.2.8"
   :description "Joplin Leiningen plugin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject joplin "0.2.8-SNAPSHOT"
+(defproject joplin "0.2.8"
   :description "Flexible datastore migration and seeding"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [joplin.core "0.2.8-SNAPSHOT"]]
+                 [joplin.core "0.2.8"]]
   :plugins [[lein-sub "0.3.0"]]
   :sub ["joplin.core" "joplin.jdbc" "joplin.elasticsearch" "joplin.zookeeper"
         "joplin.datomic" "joplin.cassandra" "joplin.hive" "joplin.lein"])
