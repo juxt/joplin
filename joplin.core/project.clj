d1500fba60dc0248625a36abd6b27a9ab006733a
Martin Trojer <martin.trojer@gmail.com>
2015-03-26 15:36:54 +0000

    back on snapshot

--- joplin.core/project.clj
+++ joplin.core/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.core "0.2.10"
+(defproject joplin.core "0.2.11-SNAPSHOT"
   :description "Flexible datastore migration and seeding"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
