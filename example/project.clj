5fa14b419249fa2dc50ce6da91386a32d9804871
Martin Trojer <martin.trojer@gmail.com>
2015-04-17 14:58:53 +0100

    back on snapshot

--- example/project.clj
+++ example/project.clj
@@ -4,9 +4,9 @@
 ;; check ES
 ;; http://192.168.59.103:9200/users/_search?pretty=true&q=*:*
 
-(defproject joplin-example "0.2.12"
+(defproject joplin-example "0.2.13-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[joplin.lein "0.2.12"]]
+  :plugins [[joplin.lein "0.2.13-SNAPSHOT"]]
 
   :source-paths ["src" "joplin"]
 
--- joplin.lein/project.clj
+++ joplin.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.lein "0.2.12"
+(defproject joplin.lein "0.2.13-SNAPSHOT"
   :description "Joplin Leiningen plugin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
