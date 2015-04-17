6cbb0da52bd214703fa8c2eac28c422c43d714f4
Martin Trojer <martin.trojer@gmail.com>
2015-04-17 14:49:33 +0100

    back on snapshot

--- joplin.elasticsearch/project.clj
+++ joplin.elasticsearch/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.elasticsearch "0.2.11"
+(defproject joplin.elasticsearch "0.2.12-SNAPSHOT"
   :description "ElasticSearch support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -7,5 +7,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clojurewerkz/elastisch "2.1.0"]
-                 [joplin.core "0.2.11"]
+                 [joplin.core "0.2.12-SNAPSHOT"]
                  [clj-time "0.9.0"]])
--- joplin.zookeeper/project.clj
+++ joplin.zookeeper/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.zookeeper "0.2.11"
+(defproject joplin.zookeeper "0.2.12-SNAPSHOT"
   :description "ZooKeeper support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -10,5 +10,5 @@
                  [org.apache.zookeeper/zookeeper "3.4.6" :exclusions [commons-codec com.sun.jmx/jmxri
                                                                       com.sun.jdmk/jmxtools javax.jms/jms
                                                                       org.slf4j/slf4j-log4j12 log4j]]
-                 [joplin.core "0.2.11"]
+                 [joplin.core "0.2.12-SNAPSHOT"]
                  [curator "0.0.2"]])
