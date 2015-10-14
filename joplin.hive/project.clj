965ac22d0a4e8bcaf241301c3e76aa666bf65d2f
Martin Trojer <martin.trojer@gmail.com>
2015-10-14 11:16:56 +0100

    back on snapshot

--- joplin.hive/project.clj
+++ joplin.hive/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.hive "0.3.4"
+(defproject joplin.hive "0.3.5-SNAPSHOT"
   :description "Hive Avro support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [joplin.core "0.3.4"]
+                 [joplin.core "0.3.5-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.4.1"]
                  [org.apache.hadoop/hadoop-client "2.7.1"]
                  [org.apache.hive/hive-exec "1.2.1"]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject joplin "0.3.4"
+(defproject joplin "0.3.5-SNAPSHOT"
   :description "Flexible datastore migration and seeding"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
