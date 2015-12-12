43f02bde245d748d31ad45a44d8ddaa199784e0a
sveri <sveri80@gmail.com>
2015-12-12 22:04:15 +0100

    pedantic again.

--- joplin.core/project.clj
+++ joplin.core/project.clj
@@ -5,7 +5,7 @@
         :url "https://github.com/juxt/joplin"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;:pedantic? :abort
+  :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.classpath "0.2.2"]
                  [clj-time "0.11.0"]
