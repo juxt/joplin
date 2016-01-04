3b228c43ddeb61c09ed5e8af233113970885cbbb
sveri <sveri80@gmail.com>
2016-01-04 15:39:52 +0100

    Providing default implementation for lein aliases.

---
+++ joplin.core/src/joplin/alias.clj
@@ -0,0 +1,53 @@
+(ns joplin.alias
+  (:require [clojure.java.io :as io]
+            [joplin.repl :as repl]))
+
+(defn load-config [path]
+  (-> (io/resource path)
+      repl/load-config))
+
+;; -------------------------------------------------------------------
+;; lein aliases
+
+;; config file is the filemae of a joplin configuration which has to recide
+;; somewhere in the resources.
+
+(defn migrate [config-file env & [db]]
+  (let [conf (load-config config-file)]
+    (if db
+      (repl/migrate conf (keyword env) (keyword db))
+      (repl/migrate conf (keyword env))))
+  (System/exit 0))
+
+(defn seed [config-file env & [db]]
+  (let [conf (load-config config-file)]
+    (if db
+      (repl/seed conf (keyword env) (keyword db))
+      (repl/seed conf (keyword env))))
+  (System/exit 0))
+
+(defn rollback [config-file env & [db num]]
+  (let [conf (load-config config-file)]
+    (when (and db num)
+      (repl/rollback conf
+                     (keyword env) (keyword db)
+                     (Long/parseLong num))))
+  (System/exit 0))
+
+(defn reset [config-file env & [db]]
+  (let [conf (load-config config-file)]
+    (when db
+      (repl/reset conf (keyword env) (keyword db))))
+  (System/exit 0))
+
+(defn pending [config-file env & [db]]
+  (let [conf (load-config config-file)]
+    (when db
+      (repl/pending conf (keyword env) (keyword db))))
+  (System/exit 0))
+
+(defn create [config-file env & [db id]]
+  (let [conf (load-config config-file)]
+    (when (and db id)
+      (repl/create conf (keyword env) (keyword db) id)))
+  (System/exit 0))
