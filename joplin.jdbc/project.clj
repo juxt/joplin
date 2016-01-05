89644f57c171b1f106aa17af6991ebc1881156bc
Martin Trojer <martin.trojer@gmail.com>
2016-01-05 12:13:16 +0000

    back on snapshot

--- joplin.jdbc/project.clj
+++ joplin.jdbc/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.jdbc "0.3.6"
+(defproject joplin.jdbc "0.3.7-SNAPSHOT"
   :description "JDBC support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [joplin.core "0.3.6"]
+                 [joplin.core "0.3.7-SNAPSHOT"]
                  [ragtime/ragtime.jdbc "0.5.2"]])
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject joplin "0.3.6"
+(defproject joplin "0.3.7-SNAPSHOT"
   :description "Flexible datastore migration and seeding"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
