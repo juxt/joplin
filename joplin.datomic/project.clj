ad76060cf5ee29f69d1aaa1f81438a577a85a1fb
madvas <matus.lestan@gmail.com>
2016-08-17 17:12:07 +0200

    v0.3.8

--- joplin.datomic/project.clj
+++ joplin.datomic/project.clj
@@ -1,4 +1,4 @@
-(defproject joplin.datomic "0.3.7"
+(defproject joplin.datomic "0.3.8"
   :description "Datomic support for Joplin"
   :url "http://github.com/juxt/joplin"
   :scm {:name "git"
@@ -6,5 +6,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [joplin.core "0.3.7"]
+                 [joplin.core "0.3.8"]
                  [com.datomic/datomic-free "0.9.5385" :exclusions [joda-time]]])
